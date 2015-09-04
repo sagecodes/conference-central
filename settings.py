8d214f052abe5d854f10985aa4b7fe2ed37d654b
Sage Elliott <wildtroll@gmail.com>
2015-09-03 17:11:27 -0700

    update client ID to be correct

--- settings.py
+++ settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'conference-elliott'
+WEB_CLIENT_ID = '92535834247-q02j3tjh5r4uulmo5r0i2erb1ug0o4vr.apps.googleusercontent.com'
 
