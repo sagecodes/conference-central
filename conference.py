14c458d9a8a0b1b3eb556e03c105162c843330fc
Sage Elliott <wildtroll@gmail.com>
2015-11-14 13:40:32 -0800

    Add _cacheAnnouncment function

--- conference.py
+++ conference.py
@@ -43,6 +43,10 @@ from utils import getUserId
 
 from settings import WEB_CLIENT_ID
 
+from google.appengine.api import memcache
+
+from models import StringMessage
+
 EMAIL_SCOPE = endpoints.EMAIL_SCOPE
 API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
 
@@ -295,7 +299,7 @@ class ConferenceApi(remote.Service):
         """Query for conferences."""
         conferences = self._getQuery(request)
 
-         # return individual ConferenceForm object per Conference
+        # return individual ConferenceForm object per Conference
         return ConferenceForms(
             items=[self._copyConferenceToForm(conf, "") \
             for conf in conferences]
@@ -436,6 +440,35 @@ class ConferenceApi(remote.Service):
         return ConferenceForms(items=[self._copyConferenceToForm(conf, "")\
          for conf in conferences]
         )
+
+    # - - - Announcements - - - - - - - - - - - - - - - - - - - -
+
+    @staticmethod
+    def _cacheAnnouncement():
+        """Create Announcement & assign to memcache; used by
+        memcache cron job & putAnnouncement().
+        """
+        confs = Conference.query(ndb.AND(
+            Conference.seatsAvailable <= 5,
+            Conference.seatsAvailable > 0)
+        ).fetch(projection=[Conference.name])
+
+        if confs:
+            # If there are almost sold out conferences,
+            # format announcement and set it in memcache
+            announcement = '%s %s' % (
+                'Last chance to attend! The following conferences '
+                'are nearly sold out:',
+                ', '.join(conf.name for conf in confs))
+            memcache.set(MEMCACHE_ANNOUNCEMENTS_KEY, announcement)
+        else:
+            # If there are no sold out conferences,
+            # delete the memcache announcements entry
+            announcement = ""
+            memcache.delete(MEMCACHE_ANNOUNCEMENTS_KEY)
+
+        return announcement
+
 
 
     # @endpoints.method(message_types.VoidMessage, ConferenceForms,
